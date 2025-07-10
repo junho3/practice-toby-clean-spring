276185ebce7aa5b922972a0cb049cd2b23385bb7
junho3 <chojunho433@gmail.com>
2025-07-10 21:08:56 +0900

    null 검증 추가

--- src/test/java/tobyspring/splearn/domain/MemberTest.java
+++ src/test/java/tobyspring/splearn/domain/MemberTest.java
@@ -3,6 +3,7 @@ package tobyspring.splearn.domain;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("Member")
@@ -14,5 +15,12 @@ class MemberTest {
         var member = new Member("test.app", "test", "password");
 
         assertThat(member.getStatus()).isEqualTo(MemberStatus.PENDING);
+    }
+
+    @Test
+    @DisplayName("생성자 NULL 체크")
+    void test2() {
+        assertThatThrownBy(() -> new Member(null, "test", "password"))
+                .isInstanceOf(NullPointerException.class);
     }
 }
