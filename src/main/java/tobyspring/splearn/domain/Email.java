53867976f1be58ddebdc2fb4d89f03de82cc7fe2
junho3 <chojunho433@gmail.com>
2025-07-14 20:13:07 +0900

    Jpa Entity로 정의

--- src/main/java/tobyspring/splearn/domain/Email.java
+++ src/main/java/tobyspring/splearn/domain/Email.java
@@ -1,7 +1,16 @@
 package tobyspring.splearn.domain;
 
+import jakarta.persistence.Embeddable;
+
 import java.util.regex.Pattern;
 
+/**
+ * 원래 @Embeddable은 기본 생성자가 필요함
+ * record는 기본 생성자를 만들어주지 않아서 jpa, hibernate 구 버전에서는 규약 위반
+ *
+ * hibernate 6.x, jpa 3.2 버전부터 record를 지원함
+ */
+@Embeddable
 public record Email(String address) {
     private static final Pattern EMAIL_PATTERN =
             Pattern.compile("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,24 +1,42 @@
 package tobyspring.splearn.domain;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import static java.util.Objects.requireNonNull;
+import static lombok.AccessLevel.PROTECTED;
 import static org.springframework.util.Assert.state;
 
+/**
+ * 코드에 어노테이션이 있다고 기술에 의존적(종속적)으로 되었는가?
+ * JPA가 없으면 사용할 수 없는 코드인가?
+ *
+ * 어노테이션의 의미는 주석
+ * 어노테이션 때문에 특별한 동작을 수행하는 프레임워크가 아니라면 기술에 의존인건 아님
+ * 순수한 도메인으로 만들었을 때와 코드가 달라져야하는데, 어노테이션만으로는 차이가 없음
+ */
 @Getter
+@Entity
 @ToString
+@NoArgsConstructor(access = PROTECTED)
 public class Member {
+    @Id
+    private Long id;
+
+    @Embedded
     private Email email;
 
     private String nickname;
 
     private String passwordHash;
 
+    @Enumerated
     private MemberStatus status;
-
-    private Member() {
-    }
 
     public static Member register(MemberRegisterRequest memberRegisterRequest, PasswordEncoder passwordEncoder) {
         Member member = new Member();
