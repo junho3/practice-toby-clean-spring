54a495cfb886e4b1c3a299d60660fc68e1c41282
junho3 <chojunho433@gmail.com>
2025-07-17 21:23:11 +0900

    mockito warning 제거를 위한 셋팅

--- src/test/java/tobyspring/splearn/SplearnApplicationTest.java
+++ src/test/java/tobyspring/splearn/SplearnApplicationTest.java
@@ -12,10 +12,13 @@ class SplearnApplicationTest {
     @Test
     @DisplayName("run()")
     void test1() {
-        MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class);
+        // static 테스트 할 때 try 블록으로 반드시 감싸줘야함
+        // 감싸지 않으면 간헐적인 테스트 실패 발생
+        try(MockedStatic<SpringApplication> mocked = Mockito.mockStatic(SpringApplication.class)) {
 
-        SplearnApplication.main(new String[0]);
+            SplearnApplication.main(new String[0]);
 
-        mocked.verify(() -> SpringApplication.run(SplearnApplication.class, new String[0]));
+            mocked.verify(() -> SpringApplication.run(SplearnApplication.class, new String[0]));
+        }
     }
 }
