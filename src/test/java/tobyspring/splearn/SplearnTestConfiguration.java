c07f99179b74f45ecdf2696462f3bc4d76ae16bf
junho3 <chojunho433@gmail.com>
2025-07-30 21:45:14 +0900

    Test 환경에서 TestConfiguration의 Bean 선언을 최우선시 하기 위한 @Primary 선언

--- src/test/java/tobyspring/splearn/SplearnTestConfiguration.java
+++ src/test/java/tobyspring/splearn/SplearnTestConfiguration.java
@@ -2,6 +2,7 @@ package tobyspring.splearn;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 import tobyspring.splearn.application.member.required.EmailSender;
 import tobyspring.splearn.domain.member.MemberFixture;
 import tobyspring.splearn.domain.member.PasswordEncoder;
@@ -11,11 +12,13 @@ public class SplearnTestConfiguration {
 
     // EmailSender가 개발된게 아니기 때문에 MemberRegister 테스트를 위해 익명클래스로 임시 구현
     @Bean
+    @Primary
     public EmailSender emailSender() {
         return (email, subject, body) -> System.out.println("Sending Email: " + email);
     }
 
     @Bean
+    @Primary
     public PasswordEncoder passwordEncoder() {
         return MemberFixture.createPasswordEncoder();
     }
