0dd88a672ec9c0ecbd3747f5cc7bb7650a7ad92d
junho3 <chojunho433@gmail.com>
2025-08-01 22:19:02 +0900

    중복 프로필 검증 구현

--- src/test/java/tobyspring/splearn/domain/member/ProfileTest.java
+++ src/test/java/tobyspring/splearn/domain/member/ProfileTest.java
@@ -2,27 +2,35 @@ package tobyspring.splearn.domain.member;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ProfileTest {
 
-    @Test
+    @ValueSource(strings = {
+            "xxxxx",
+            "xxxxx123456",
+            "123456",
+            ""
+    })
+    @ParameterizedTest
     @DisplayName("Profile은 15자리 미만 영문과 숫자만 가능")
-    void test1 () {
-        new Profile("xxxxx");
-        new Profile("xxxxx");
-        new Profile("123456");
+    void test1 (String address) {
+        new Profile(address);
     }
 
-    @Test
+    @ValueSource(strings = {
+            "12345678901234567",
+            "XXXX",
+            "프로필"
+    })
+    @ParameterizedTest
     @DisplayName("Profile 실패 케이스")
-    void test2() {
-        assertThatThrownBy(() -> new Profile("")).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Profile("12345678901234567")).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Profile("XXXX")).isInstanceOf(IllegalArgumentException.class);
-        assertThatThrownBy(() -> new Profile("프로필")).isInstanceOf(IllegalArgumentException.class);
+    void test2(String address) {
+        assertThatThrownBy(() -> new Profile(address)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
