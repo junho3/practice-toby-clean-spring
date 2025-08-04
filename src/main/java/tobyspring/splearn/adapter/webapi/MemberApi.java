c8bebd9252104a77c39c82c01a452f27e3d4d91b
junho3 <chojunho433@gmail.com>
2025-08-04 21:18:07 +0900

    주석 추가

--- src/main/java/tobyspring/splearn/adapter/webapi/MemberApi.java
+++ src/main/java/tobyspring/splearn/adapter/webapi/MemberApi.java
@@ -16,6 +16,11 @@ public class MemberApi {
 
     private final MemberRegister memberRegister;
 
+    /**
+     * domain에 위치한 MemberRegisterRequest를 직접 사용하는 것은 의아함
+     * 1. web 레이어에서만 필요한 어노테이션이 추가 될 수 있음
+     * 2. memberRegister는 web 외에도 다른 곳에서 호출될 수 있는데, MemberRegisterRequest를 직접 사용하면 web과 강결합이 됨
+     */
     @PostMapping("/api/v1/members")
     public MemberRegisterResponse register(@Valid @RequestBody MemberRegisterRequest request) {
         final Member member = memberRegister.register(request);
